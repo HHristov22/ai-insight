8ebe8523f52fe28b58b0da764fbbe48b2937230f
hhristov22 <h.hristov2212@gmail.com>
2024-11-25 18:07:51 +0200

    Contact us functionality

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import { Container, Typography, Box, TextField, Button, Paper } from '@mui/material';
 import Layout from '../../components/layout/Layout';
+import emailjs from "@emailjs/browser";
 
 export default function Contact({ darkMode, toggleDarkMode }) {
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Add contact form submission logic here
+
+    const templateParams = {
+      from_name: event.target.name.value,
+      user_email: event.target.email.value,
+      message: event.target.message.value,
+    };
+
+    emailjs
+      .send(
+        "service_w7ohii5",
+        "template_p93tp4f",
+        templateParams,
+        "yRrTnmnow7ooMfiv_"
+      )
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+          alert("Message sent successfully!");
+        },
+        (err) => {
+          console.error("FAILED...", err);
+          alert("Message failed to send.");
+        }
+      );
   };
 
   return (
     <Layout darkMode={darkMode} toggleDarkMode={toggleDarkMode}>
       <Container maxWidth="md" sx={{ my: 4 }}>
-        <Typography variant="h3" component="h1" sx={{ mb: 4 }}>
-          Contact Us
+        <Typography variant="h4" component="h1" sx={{ mb: 4 }}>
+        Contact Me
         </Typography>
         <Paper sx={{ p: 4 }}>
           <Box component="form" onSubmit={handleSubmit} noValidate>
